819c45fcd296113bb920f3e9c1eaaded157eb85b
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2025-05-25 14:37:56 +0200

    Struktur uppbyggd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-
 function App() {
-  
-
   return (
     <>
-      
+      <div>
+        <h1> Vad skall jag göra idag? </h1>
+      </div>
+      <div>
+        <h5>Jag vill vara ....</h5>
+        <div>
+          <input type="checkbox" />
+          <label>Inomhus</label>
+          <input type="checkbox" />
+          <label>Utomhus</label>
+        </div>
+        <div>
+          <input type="checkbox" />
 
+          <label>Hemma</label>
+          <input type="checkbox" />
+          <label>Åka iväg</label>
+        </div>
+      </div>
+      <div>
+        <button> Slumpa </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
