4d1c991e5403492ad54bb7806969bdd19e083324
natalie.hallerdal@sensera.se <natalie.hallerdal@yahoo.se>
2025-05-27 16:39:52 +0200

    Added aktivitis in db

--- src/data/aktiviteter.jsx
+++ src/data/aktiviteter.jsx
@@ -1,39 +1,131 @@
-export const aktiviteter = [
 
-    {
-        text: "Läs en bok",
-        plats: "inne",
-        läge: "hemma",
-        budget: "gratis"
-    },
-    {
-        text: "Besök ett cafe",
-        plats: "inne",
-        läge: "borta",
-        budget: "small"
-    },
-    {
-        text: "Prommenera i skogen",
-        plats: "ute",
-        läge: "borta",
-        budget: "gratis"
-    },
-    {
-        text: "Åk och gör en heldag på ett nöljesfält",
-        plats: "ute",
-        läge: "borta",
-        budget: "medium"
-    },
-    {
-        text: "Heldag på spa",
-        plats: "inne",
-        läge: "borta",
-        budget: "high"
-    },
-    {
-        text: "Filmmaraton",
-        plats: "inne",
-        läge: "Hemma",
-        budget: "gratis"
-    },
-]
+
+    export const aktiviteter = [
+        {
+          text: "Läs en bok",
+          plats: "inne",
+          läge: "hemma",
+          budget: "gratis",
+        },
+        {
+          text: "Besök ett café",
+          plats: "inne",
+          läge: "borta",
+          budget: "small",
+        },
+        {
+          text: "Promenera i skogen",
+          plats: "ute",
+          läge: "borta",
+          budget: "gratis",
+        },
+        {
+          text: "Åk och gör en heldag på ett nöjesfält",
+          plats: "ute",
+          läge: "borta",
+          budget: "medium",
+        },
+        {
+          text: "Heldag på spa",
+          plats: "inne",
+          läge: "borta",
+          budget: "high",
+        },
+        {
+          text: "Filmmaraton",
+          plats: "inne",
+          läge: "hemma",
+          budget: "gratis",
+        },
+        {
+          text: "Bygg en koja inomhus",
+          plats: "inne",
+          läge: "hemma",
+          budget: "gratis",
+        },
+        {
+          text: "Testa ett nytt recept",
+          plats: "inne",
+          läge: "hemma",
+          budget: "small",
+        },
+        {
+          text: "Gå till en secondhandbutik",
+          plats: "inne",
+          läge: "borta",
+          budget: "small",
+        },
+        {
+          text: "Besök ett museum",
+          plats: "inne",
+          läge: "borta",
+          budget: "medium",
+        },
+        {
+          text: "Måla eller rita något",
+          plats: "inne",
+          läge: "hemma",
+          budget: "gratis",
+        },
+        {
+          text: "Picknick i en park",
+          plats: "ute",
+          läge: "borta",
+          budget: "small",
+        },
+        {
+          text: "Fota naturen",
+          plats: "ute",
+          läge: "hemma",
+          budget: "gratis",
+        },
+        {
+          text: "Bada i en sjö",
+          plats: "ute",
+          läge: "borta",
+          budget: "gratis",
+        },
+        {
+          text: "Resa till annan stad över dagen",
+          plats: "ute",
+          läge: "borta",
+          budget: "medium",
+        },
+        {
+          text: "Boka en weekend på hotell",
+          plats: "inne",
+          läge: "borta",
+          budget: "high",
+        },
+        {
+          text: "Sortera en låda du skjutit upp",
+          plats: "inne",
+          läge: "hemma",
+          budget: "gratis",
+        },
+        {
+          text: "Ha en lyxfrukost hemma med tända ljus",
+          plats: "inne",
+          läge: "hemma",
+          budget: "small",
+        },
+        {
+          text: "Besök ett escape room",
+          plats: "inne",
+          läge: "borta",
+          budget: "medium",
+        },
+        {
+          text: "Åk på ridtur i naturen",
+          plats: "ute",
+          läge: "borta",
+          budget: "high",
+        },
+        {
+          text: "Ligg på gräset och titta på molnen",
+          plats: "ute",
+          läge: "hemma",
+          budget: "gratis",
+        }
+      ];
+      
